b2d7f975d5b48e6ae5ef477fcaa37450c9536006
riccardotornesello <riccardo.tornesello@gmail.com>
2021-04-19 17:30:10 +0200

    install vpn

--- main.py
+++ main.py
@@ -50,3 +50,31 @@ if __name__ == "__main__":
 
     # Generate hosts inventory
     init.generate_hosts(config["hosts"]["manager"], config["hosts"]["vulnboxes"])
+
+    # Install OpenVPN server on master
+    playbook = [{
+        "name": "Install the VPN server",
+        "hosts": "manager",
+        "become": "yes",
+        "roles": [{
+            "role": "kyl191.openvpn",
+            "clients": ["vpn"],
+            "openvpn_port": config["vpn"]["port"],
+            "openvpn_duplicate_cn": "true",
+            "openvpn_server_network": config["vpn"]["network"][:-3], # TODO: get subnet from here
+            "openvpn_server_netmask": "255.255.255.0",
+            "openvpn_fetch_config_dir": "../",
+            "openvpn_push": [f"route {config['networks'][0]['cidr'][:-3]} 255.255.255.0"] # TODO: select variable subnet mask
+        }]
+    }]
+    with open("ansible/project/vpn.yml", 'w') as f:
+        yaml.dump(playbook, f)
+
+    # Execute the vpn installation
+    r = ansible_runner.run(
+        private_data_dir=settings.PRIVATE_DATA_DIR,
+        playbook='vpn.yml'
+    )
+    print("{}: {}".format(r.status, r.rc))
+    print("Final status:")
+    print(r.stats)
